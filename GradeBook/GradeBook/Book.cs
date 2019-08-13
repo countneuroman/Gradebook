953427597103438a048534473b1621729fa8a4bf
Vadim Alekseev <51336503+countneuroman@users.noreply.github.com>
2019-08-13 20:45:14 +0300

    Add main logic

---
+++ GradeBook/GradeBook/Book.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GradeBook
+{
+    class Book
+    {
+        public void ShowStatistics()
+        {
+            var amount = 0.0;
+            var highGrade = double.MinValue;
+            var lowGrade = double.MaxValue;
+            foreach (var number in grades)
+            {
+                highGrade = Math.Max(number, highGrade);
+                lowGrade = Math.Min(number, lowGrade);
+                amount += number;
+            }
+            double result = amount / grades.Count;
+            Console.WriteLine(highGrade);
+            Console.WriteLine(lowGrade);
+            Console.WriteLine(result);
+        }
+        public Book(string name)
+        {
+            grades = new List<double>();
+            this.name = name;
+        }
+        public void AddGrade(double grade)
+        {
+            grades.Add(grade);
+        }
+
+        private List<double> grades;
+        private string name;
+    }
+}
