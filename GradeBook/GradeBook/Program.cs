6af874fb37e5c24f1281db23db4299404d980e47
Vadim Alekseev <51336503+countneuroman@users.noreply.github.com>
2019-08-18 14:48:49 +0300

    input user ratings

--- GradeBook/GradeBook/Program.cs
+++ GradeBook/GradeBook/Program.cs
@@ -8,16 +8,29 @@ namespace GradeBook
         static void Main(string[] args)
         {
             var book = new Book("Vadim Gradebook");
-            book.AddGrade(89.1);
-            book.AddGrade(90.5);
-            book.AddGrade(77.5);
 
-            var stats = book.GetStatistics();
+            var done = false;
 
-            Console.WriteLine($"Минимальная оценка равна {stats.Low}");
-            Console.WriteLine($"Максимальная оценка равна {stats.High}");
-            Console.WriteLine($"Средняя оценка равна {stats.Average:N1}");
-            Console.WriteLine($"Буквенная средняя оценка равна {stats.Letter}");
+            while (!done)
+            {
+                Console.WriteLine("Введи число или 'q' для выхода");
+                var input = Console.ReadLine();
+
+                if (input == "q")
+                {
+                    done = true;
+                    continue;
+                }
+                var grade = double.Parse(input);
+                book.AddGrade(grade);
+            }
+
+                var stats = book.GetStatistics();
+
+                Console.WriteLine($"Минимальная оценка равна {stats.Low}");
+                Console.WriteLine($"Максимальная оценка равна {stats.High}");
+                Console.WriteLine($"Средняя оценка равна {stats.Average:N1}");
+                Console.WriteLine($"Буквенная средняя оценка равна {stats.Letter}");
 
             Console.ReadKey();
         }
