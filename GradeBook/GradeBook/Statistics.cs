e9b5c901f11640d7cca988c31f2dc6411432c9f7
Vadim Alekseev <olorinoren@gmail.com>
2019-09-07 13:59:50 +0300

    Refactoring code

--- GradeBook/GradeBook/Statistics.cs
+++ GradeBook/GradeBook/Statistics.cs
@@ -11,4 +11,96 @@ namespace GradeBook
         public double Low;
         public char Letter;
     }
+
+    public class inMemoryBook : Book
+    {
+        public inMemoryBook(string name) : base(name)
+        {
+            grades = new List<double>();
+            Name = name;
+        }
+
+
+        public void AddGrade(char letter)
+        {
+            switch (letter)
+            {
+                case 'A':
+                    AddGrade(90);
+                    break;
+                case 'B':
+                    AddGrade(80);
+                    break;
+                case 'C':
+                    AddGrade(70);
+                    break;
+                case 'D':
+                    AddGrade(60);
+                    break;
+                case 'F':
+                    AddGrade(50);
+                    break;
+                default:
+                    AddGrade(0);
+                    break;
+            }
+        }
+
+        public override void AddGrade(double grade)
+        {
+            if (grade <= 100 && grade >= 0)
+            {
+                grades.Add(grade);
+                if (GradeAdded != null)
+                {
+                    GradeAdded(this, new EventArgs());
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid value!");
+            }
+        }
+
+        public override event GradeAddedDelegate GradeAdded;
+        public override Statistics GetStatistics()
+        {
+            var result = new Statistics();
+            result.Average = 0.0;
+            result.High = double.MinValue;
+            result.Low = double.MaxValue;
+
+            for (var index = 0; index < grades.Count; index++)
+            {
+                result.High = Math.Max(grades[index], result.High);
+                result.Low = Math.Min(grades[index], result.Low);
+                result.Average += grades[index];
+            }
+            result.Average /= grades.Count;
+
+            switch (result.Average)
+            {
+                case var d when d >= 90:
+                    result.Letter = 'A';
+                    break;
+                case var d when d >= 80:
+                    result.Letter = 'B';
+                    break;
+                case var d when d >= 70:
+                    result.Letter = 'C';
+                    break;
+                case var d when d >= 60:
+                    result.Letter = 'D';
+                    break;
+                default:
+                    result.Letter = 'F';
+                    break;
+            }
+
+            return result;
+        }
+
+
+        private List<double> grades;
+    }
 }
